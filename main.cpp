b5ebd5f9dc47fdb645fc75a52995221c0f2798e1
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-07-04 07:04:48 +0700

    📦 Added manual test

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file main.cpp
+ * @author UnknownRori (you@domain.com)
+ * @brief This is place for manual testing
+ * @version 0.1
+ * @date 2022-07-04
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <iostream>
+#include "./include/UnknownRori.hpp"
+
+int main()
+{
+    return 0;
+}
