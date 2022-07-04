33a05f1421db3d3411f32da13563839f1c3c7548
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-07-05 06:15:32 +0700

    📦 Implement UnknownRori_New.hpp

---
+++ include/UnknownRori_New.hpp
@@ -0,0 +1,37 @@
+/**
+ * @file UnknownRori_New.hpp
+ * @author UnknownRori
+ * @brief A overloaded version of new keyword for debugging purposes
+ * @version 0.1
+ * @date 2022-07-04
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#pragma once
+
+#ifndef UNKNOWNRORI_NEW_HPP
+#define UNKNOWNRORI_NEW_HPP
+
+#include <iostream>
+
+static size_t __ALLOCATED_TIME = 0;
+
+void *operator new(size_t size)
+{
+    __ALLOCATED_TIME++;
+    std::cout << "Allocated!"
+              << "\n";
+    return malloc(size);
+}
+
+void *operator new[](size_t size)
+{
+    __ALLOCATED_TIME++;
+    std::cout << "Allocated!"
+              << "\n";
+    return malloc(size);
+}
+
+#endif
